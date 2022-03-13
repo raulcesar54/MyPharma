93b9b7ca27c711cb879071439f5633595f74050a
raulferreira <raulferreira@grpereira.com.br>
2022-03-12 21:33:39 -0400

    feat:Created product create route

---
+++ src/controller/product/index.ts
@@ -0,0 +1,126 @@
+import { Request, Response } from 'express'
+import { ProductsModel } from '../../model/products'
+
+export const Product = {
+  insert: async (request: Request, response: Response) => {
+    try {
+      const {
+        body: { name, price, stock, category, mark, description },
+      } = request
+
+      if (!name) throw 'Name is required!'
+      if (!price) throw 'Price is required!'
+      if (!stock) throw 'Stock is required!'
+      if (!category) throw 'Category is required!'
+      if (!mark) throw 'Mark is required!'
+      if (!description) throw 'Description is required!'
+
+      const insertItems = new ProductsModel({
+        name,
+        price,
+        stock,
+        category,
+        mark,
+        description,
+      })
+      await insertItems.save()
+
+      return response.json({
+        message: 'New item inserted sucessfull!',
+        done: true,
+        data: { name, description },
+      })
+    } catch (err) {
+      return response.status(400).json({ message: err, done: false })
+    }
+  },
+  // update: async (request: Request, response: Response) => {
+  //   try {
+  //     const {
+  //       body: { name, description },
+  //       params: { id },
+  //     } = request
+
+  //     const findById = await ProductsCategoryModel.findOneAndUpdate(
+  //       { id },
+  //       {
+  //         name,
+  //         description,
+  //       }
+  //     )
+
+  //     if (!findById) throw 'Id not exist!'
+  //     const findItem = await ProductsCategoryModel.findOne({ id })
+
+  //     return response.json({
+  //       message: 'Item updated sucessfull!',
+  //       done: true,
+  //       data: findItem,
+  //     })
+  //   } catch (err) {
+  //     return response.status(400).json({ message: err, done: false })
+  //   }
+  // },
+  // delete: async (request: Request, response: Response) => {
+  //   try {
+  //     const {
+  //       params: { id },
+  //     } = request
+  //     const findAndRemoveItem = await ProductsCategoryModel.findOneAndDelete({
+  //       id,
+  //     })
+  //     if (!findAndRemoveItem) throw 'Id not exist!'
+  //     return response.json({
+  //       message: 'Item deleted sucessfull!',
+  //       done: true,
+  //     })
+  //   } catch (err) {
+  //     return response.status(400).json({ message: err, done: false })
+  //   }
+  // },
+  // list: async (request: Request, response: Response) => {
+  //   const {
+  //     query: { search, name, description },
+  //   } = request
+  //   const data = await Product.filter({
+  //     search: String(search),
+  //     name: String(name),
+  //     description: String(description),
+  //   })
+
+  //   return response.json({
+  //     done: true,
+  //     qty: data.length,
+  //     data,
+  //   })
+  // },
+  // filter: async ({
+  //   search,
+  //   name,
+  //   description,
+  // }: {
+  //   search?: string
+  //   name?: string
+  //   description?: string
+  // }) => {
+  //   if (search != 'undefined') {
+  //     return await ProductsCategoryModel.find({
+  //       $or: [
+  //         { name: { $regex: `.*${search}.*` } },
+  //         { description: { $regex: `.*${search}.*` } },
+  //       ],
+  //     })
+  //   }
+  //   if (name !== 'undefined') {
+  //     return await ProductsCategoryModel.find({
+  //       name: { $regex: `.*${name}.*` },
+  //     })
+  //   }
+  //   if (description !== 'undefined') {
+  //     return await ProductsCategoryModel.find({
+  //       description: { $regex: `.*${description}.*`, $options: 'i' },
+  //     })
+  //   }
+  //   return await ProductsCategoryModel.find()
+  // },
+}
--- src/model/products/index.ts
+++ src/model/products/index.ts
@@ -6,8 +6,8 @@ const schema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   stock: { type: Number, required: true },
-  category: { type: Number, required: true },
-  mark: { type: Number, required: true },
+  category: [{ type: mongoose.Schema.Types.ObjectId, ref: 'productsCategory' }],
+  mark: [{ type: mongoose.Schema.Types.ObjectId, ref: 'mark' }],
   description: { type: String, required: true },
 })
 
